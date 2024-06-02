7da8bb559bc474defe1c2063b4fcc7e09acd0e38
badiabouziad <badi3214932002>
2024-06-02 11:13:05 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import "./App.css";
+import Cursor from "./Components/Cursor/Cursor";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+import ProjectsSlider from "./Components/ProjectsSlider/ProjectsSlider";
+import About from "./Components/About/About";
+function App() {
+  return (
+    <>
+      <div className="background" id="background">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <Cursor />
+      <Navbar />
+      <Hero />
+      <ProjectsSlider />
+      <About />
+
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <h1>Hi farah</h1>
+    </>
+  );
+}
+
+export default App;
