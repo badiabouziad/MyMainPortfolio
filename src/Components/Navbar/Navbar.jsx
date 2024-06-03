b04a03ab05c8d53cadae980375da12ee49990289
Badia bouziad <101726929+badiabouziad@users.noreply.github.com>
2024-06-03 12:44:46 +0100

    .

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import { Link } from "react-scroll";
+import { CgDarkMode } from "react-icons/cg";
 import "./Navbar.css";
 const Navbar = () => {
   useEffect(() => {
@@ -156,43 +157,8 @@ const Navbar = () => {
           className={`mode ${darkMode ? "" : "mode-inactif"} light-btn`}
           onClick={toggleDarkMode}
         >
-          <div
-            className="mode-light"
-            id="mode"
-            onMouseOver={magnify3}
-            onMouseOut={removeMagnify3}
-          >
-            <svg
-              width="64px"
-              height="64px"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              transform="matrix(1, 0, 0, 1, 0, 0)"
-            >
-              <g id="SVGRepo_bgCarrier" strokeWidth="0" />
-
-              <g
-                id="SVGRepo_tracerCarrier"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-
-              <g id="SVGRepo_iconCarrier">
-                {" "}
-                <path
-                  d="M12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8V16Z"
-                  fill="#fff"
-                />{" "}
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM12 4V8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16V20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4Z"
-                  fill="#fff"
-                />{" "}
-              </g>
-            </svg>
-          </div>
+         <CgDarkMode  onMouseOver={magnify3}
+          onMouseOut={removeMagnify3}/>
         </button>
       </div>
       <nav className="navbar" id="navbar">
